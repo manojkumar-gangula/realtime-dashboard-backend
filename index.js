253ad3d1cd0733e31e9aa3f9089333c994aea884
Manoj Kumar Gangula <mkgangula1@gmail.com>
2025-05-08 00:53:30 +0530

    Room functionality addition

--- index.js
+++ index.js
@@ -6,17 +6,38 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
-
-app.get("/", (req, res) => {
-  const dir_name = dirname(fileURLToPath(import.meta.url));
-  res.sendFile(join(dir_name, "index.html"));
+// 1 -> 1 Mapping, one socket can be only one room.
+const socketToRoom = new Map();
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log("user connected");
-  socket.on("message", (msg) => {
-    socket.broadcast.emit("message", msg);
+  // console.log("user connected, socket: " + JSON.stringify(socket));
+  // console.log(util.inspect(socket, { depth: null, colors: true }));
+  //Receiving messages from Client
+  socket.on("send_msg", (msg) => {
+    socket.to(socketToRoom.get(socket.id)).emit("receive_msg", msg);
+  });
+
+  //Joining room
+  socket.on("join_room", (roomId) => {
+    console.log("Joined room");
+    //We are maintaining 1 -> 1 mapping. Meaning one socket is associated with only one Room.
+    socketToRoom.set(socket.id, roomId);
+    console.log(JSON.stringify(socketToRoom));
+
+    socket.join(roomId);
+  });
+
+  //Leaving room
+  socket.on("leave_room", (roomId) => {
+    console.log("Left room");
+    socketToRoom.delete(socket.id);
+    socket.leave(roomId);
   });
 });
 
